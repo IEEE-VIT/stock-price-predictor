7b8314842bfef95ddadfccccc66f459736f8b2fa
utkarsh01-byte <utkarshvikram01@gamil.com>
2025-09-22 15:48:33 +0530

    solved

--- predictor.py
+++ predictor.py
@@ -3,9 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
-
 data = pd.read_csv("AAPL.csv") 
-
 data['Date'] = pd.to_datetime(data['Date']) 
 data = data.dropna(subset=['Date', 'Close'])
 data = data.sort_values('Date')
@@ -18,11 +16,15 @@ y = data['Close']
 model = LinearRegression()
 model.fit(X, y)
 
-future_days = np.arange(len(data)+1, len(data)+6).reshape(-1,1)
+future_days = np.arange(len(data) + 1, len(data) + 6).reshape(-1, 1)
 preds = model.predict(future_days)
 
 print("Predictions for next 5 days:", preds)
 
-plt.scatter(X, y, color="blue")
-plt.plot(future_days["day"], preds, color="red")
+plt.scatter(X, y, color="blue", label="Actual")
+plt.plot(future_days, preds, color="red", marker='o', label="Predicted")
+plt.xlabel('Day')
+plt.ylabel('Close Price')
+plt.title('AAPL Stock Price Prediction for Next 5 Days')
+plt.legend()
 plt.show()
