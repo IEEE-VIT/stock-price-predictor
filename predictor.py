844db6752bc84f267f866244e4aea8fdac690e9d
k3shavv <keshavkumaralg@gmail.com>
2025-09-22 17:01:08 +0530

    Add train/test split for validation #2

--- predictor.py
+++ predictor.py
@@ -2,7 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
-
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
 
 data = pd.read_csv("AAPL.csv") 
 
@@ -12,11 +13,18 @@ data = data.sort_values('Date')
 
 data['Day'] = range(1, len(data) + 1) 
 
-X = data['Day']
+X = data['Day'].values.reshape(-1, 1)
 y = data['Close']
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
 model = LinearRegression()
-model.fit(X, y)
+model.fit(X_train, y_train)
+
+test_preds = model.predict(X_test)
+
+mae = mean_absolute_error(y_test, test_preds)
+print(f"Model Mean Absolute Error on Test Data: ${mae:.2f}")
 
 future_days = np.arange(len(data)+1, len(data)+6).reshape(-1,1)
 preds = model.predict(future_days)
