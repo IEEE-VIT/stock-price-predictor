ae88019931e7508bb2c075dbab8dd57578eac11f
utkarsh01-byte <utkarshvikram01@gamil.com>
2025-09-22 16:10:10 +0530

    solved all errors

--- predictor.py
+++ predictor.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 
 data = pd.read_csv("AAPL.csv") 
 data['Date'] = pd.to_datetime(data['Date']) 
@@ -13,18 +14,28 @@ data['Day'] = range(1, len(data) + 1)
 X = data[['Day']]
 y = data['Close']
 
+split_idx = int(len(data) * 0.8)
+X_train, X_test = X.iloc[:split_idx], X.iloc[split_idx:]
+y_train, y_test = y.iloc[:split_idx], y.iloc[split_idx:]
+
 model = LinearRegression()
-model.fit(X, y)
+model.fit(X_train, y_train)
 
-future_days = np.arange(len(data) + 1, len(data) + 6).reshape(-1, 1)
-preds = model.predict(future_days)
+y_pred_test = model.predict(X_test)
+mse = mean_squared_error(y_test, y_pred_test)
+print(f"Mean Squared Error on test set: {mse}")
 
-print("Predictions for next 5 days:", preds)
+future_days = np.arange(len(data) + 1, len(data) + 6)
+future_days = future_days.reshape(-1, 1)
+future_preds = model.predict(future_days)
 
-plt.scatter(X, y, color="blue", label="Actual")
-plt.plot(future_days, preds, color="red", marker='o', label="Predicted")
+print("Predictions for next 5 days:", future_preds)
+
+plt.scatter(X_train, y_train, color="blue", label="Train Data")
+plt.scatter(X_test, y_test, color="green", label="Test Data")
+plt.plot(future_days, future_preds, color="red", marker='o', label="Future Predictions")
 plt.xlabel('Day')
 plt.ylabel('Close Price')
-plt.title('AAPL Stock Price Prediction for Next 5 Days')
+plt.title('AAPL Stock Price Prediction with Train/Test Split')
 plt.legend()
 plt.show()
